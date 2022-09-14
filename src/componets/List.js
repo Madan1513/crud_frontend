b7acf5975a03a9ebecd243f73f746c0db2a1575e
Madan1513 <madan.md@gmail.com>
2022-09-14 11:22:48 +0530

    UI element for initial view along with the data representaion.

---
+++ src/componets/List.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ListItem from "./ListItem";
+
+const List = () => {
+  const [employees, setEmployees] = useState([]);
+
+  const fetchAllRecords = () => {
+    axios.get(`${process.env.REACT_APP_API_PATH}/employees`).then((res) => {
+      if (res && res.data) {
+        setEmployees(res.data);
+      }
+    });
+  };
+
+  useEffect(() => fetchAllRecords(), []);
+
+  return (
+    <div className="emp-details">
+      <table>
+        <thead>
+          <tr>
+            <th>Employee Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Email ID</th>
+            <th>Phone No.</th>
+            <th>Edit</th>
+            <th>Delete</th>
+            <th>Completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees &&
+            employees.length > 0 &&
+            employees.map((emp, i) => {
+              return <ListItem  key={i + "emp"} data={emp} />;
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default List;
